4d69ac8d43dbb9324927bbd7c8ea1ae6203305a3
Karan Garg <karan018522@gmail.com>
2025-03-26 16:57:42 +0530

    templates tutorial

--- Backend/myProject/myapp/urls.py
+++ Backend/myProject/myapp/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
+    path('', views.home, name = 'home'),
     path('about/', views.about, name = 'about'),
-    path('home/', views.home, name = 'home'),
-    # path('counter', views.counter, name = 'counter')
 ]
--- Backend/myProject/myapp/views.py
+++ Backend/myProject/myapp/views.py
@@ -3,23 +3,8 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1> HOME PAGE</h1>')
+    return render(request, 'index.html')
 
 
 def about(request):
-    return HttpResponse('<h1> About </h1>')
-
-def index(request):
-    context = {
-        'name' : 'Peter',
-        'Power' : 'Spider-man'
-    }
-
-    return render(request, 'index.html', context)
-'''
-
-def counter(request):
-    words = request.POST['words']
-    num_words = len(words.split())
-    return render(request, 'counter.html', {'number_of_words': num_words})
-'''
+    return HttpResponse('<h1> This is the About Page </h1>')
